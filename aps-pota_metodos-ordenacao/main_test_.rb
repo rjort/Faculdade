9e16219772fbbdfe136cf33e677bcba1ff6bf1fe
Reuter Junior <reuter_junior@hotmail.com>
2019-10-09 09:57:12 -0300

    adicionado em 'main_test' funcionalidade de banchmark

--- aps-pota_metodos-ordenacao/main_test_.rb
+++ aps-pota_metodos-ordenacao/main_test_.rb
@@ -1,52 +1,28 @@
 # Todos os testes devem ser feitos nesse arquivo
-
+require 'benchmark'
 require_relative './controllers/application_controller.rb'
 
+def organize_methods(obj,vetor)
+  obj.context_save(1)
+  obj.bubble_sort_init(vetor)
+  obj.bucket_sort_init(vetor)
+  obj.count_sort_init(vetor)
+  obj.heap_sort_init(vetor)
+  obj.insertion_sort_init(vetor)
+  obj.merge_sort_init(vetor)
+  obj.quick_sort_init(vetor)
+  obj.radix_sort_init(vetor)
+  obj.selection_sort_init(vetor)
+end
+
 obj = SortMethods.new
-obj.context_save(1)
-obj.bubble_sort_init([3,4,10,1,100,32,2])
-obj.print_array_sorted
-obj.print_count
-puts ""
 
-obj.bucket_sort_init([3,4,10,1,100,32,2])
-obj.print_array_sorted
-obj.print_count
-puts ""
-
-obj.count_sort_init([3,4,10,1,100,32,2])
-obj.print_array_sorted
-obj.print_count
-puts ""
-
-obj.heap_sort_init([3,4,10,1,100,32,2])
-obj.print_array_sorted
-obj.print_count
-puts ""
-
-obj.insertion_sort_init([3,4,10,1,100,32,2])
-obj.print_array_sorted
-obj.print_count
-puts ""
-
-obj.merge_sort_init([3,4,10,1,100,32,2])
-obj.print_array_sorted
-obj.print_count
-puts ""
-
-obj.quick_sort_init([3,4,10,1,100,32,2])
-obj.print_array_sorted
-obj.print_count
-puts ""
-
-obj.radix_sort_init([3,4,10,1,100,32,2])
-obj.print_array_sorted
-obj.print_count
-puts ""
-
-obj.selection_sort_init([3,4,10,1,100,32,2])
-obj.print_array_sorted
-obj.print_count
-puts ""
-
-obj.print_statistics
+Benchmark.bm do |bck|
+  bck.report do
+    (1..5).each do |i|
+      puts "Loop #{i}"
+      vetor = Array.new(10000) { |nums| nums = rand(10000) }
+      organize_methods(obj,vetor)
+    end
+  end
+end
